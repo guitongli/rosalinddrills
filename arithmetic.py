5c2120b5c7cf69cf5653fcff89bc63f60b72c07b
guitongli <guitong.lee.contact@gmail.com>
2021-08-21 21:16:53 +0200

    till file reading

---
+++ arithmetic.py
@@ -0,0 +1,24 @@
+#1
+
+pinrt (995**2+ 894**2)
+
+#2
+
+s='rNqQtWs3qtj0ScPTEF0wZfGPkAiz4s2NjVJFdBK68gGUOUE3Candoia99p60sJxV43I61TJlCPVYp9rF5yfUo90lQfpIdi9rTejclv53gpVL818yzyLk5fnbyJleTEn6HcPLxnci7jgDxLvwIfKbWcqekPkP3LdnBk8sxgoZussuriensisiOlAFB6r6PHHpBy.'
+a=22
+b=27
+c=97
+d=102
+print ([s[a:b+1],s[c:d+1]])
+
+
+#3
+sum=0
+for num in range (4481, 9145):
+ if num%2==1:
+  sum=sum+num
+
+print(sum)
+
+#4
+
